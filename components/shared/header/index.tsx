a6e8263cc820a4f1686568d41a526ddeb44848ef
CatAvadani <catalinaavadani@gmail.com>
2025-04-18 16:44:45 +0200

    Refactor header component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,4 @@
 import { APP_NAME } from "@/lib/constants";
-import Image from "next/image";
 import Link from "next/link";
 import Menu from "./menu";
 
@@ -9,17 +8,28 @@ const Header = () => {
       <div className="wrapper flex-between">
         <div className="flex-start">
           <Link href="/" className=" flex-start">
-            <Image
+            {/* <Image
               src="/images/logo.png"
               alt={`${APP_NAME}logo`}
               height={48}
               width={48}
               property="true"
               className="rounded-full object-cover border-2 border-indigo-800"
-            />
-            <span className="hidden lg:block font-bold text-2xl ml-3">
+            /> */}
+            <h1 className="hidden lg:block font-bold text-2xl ml-3">
               {APP_NAME}
-            </span>
+            </h1>
+            <ul className="flex gap-6 ml-10">
+              <Link href="/" className=" hover:text-gray-500">
+                Men
+              </Link>
+              <Link href="/" className="hover:text-gray-500">
+                Women
+              </Link>
+              <Link href="/" className="hover:text-gray-500">
+                Kids
+              </Link>
+            </ul>
           </Link>
         </div>
         <Menu />
