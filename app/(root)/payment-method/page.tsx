48f78b95b8298386d7e01b0f4f2929ddb99af74d
CatAvadani <catalinaavadani@gmail.com>
2025-04-20 11:19:31 +0200

    Add CheckoutSteps component

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,5 +1,12 @@
+import CheckoutSteps from "@/components/shared/checkoutSteps";
+
 const PaymentMethod = () => {
-  return <div>Payment Method</div>;
+  return (
+    <div>
+      <CheckoutSteps current={2} />
+      <h1 className=" h3-bold text-center">Payment Method</h1>
+    </div>
+  );
 };
 
 export default PaymentMethod;
