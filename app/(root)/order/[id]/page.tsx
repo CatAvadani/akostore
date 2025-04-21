0a95d886faffae8e73bb5ba81a134b43a9c4c9b3
CatAvadani <catalinaavadani@gmail.com>
2025-04-21 20:14:54 +0200

    Add order details page

---
+++ app/(root)/order/[id]/page.tsx
@@ -0,0 +1,17 @@
+import { getOrderById } from "@/lib/actions/order.actions";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Order Details",
+};
+const OrderDetails = async (props: { params: Promise<{ id: string }> }) => {
+  const { id } = await props.params;
+
+  const order = await getOrderById(id);
+  if (!order) notFound();
+
+  return <div>Order Details</div>;
+};
+
+export default OrderDetails;
