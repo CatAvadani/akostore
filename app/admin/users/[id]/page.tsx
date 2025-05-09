5e39f7657fa2cacf0a8367999221216efeed2d0b
CatAvadani <catalinaavadani@gmail.com>
2025-05-09 14:21:50 +0200

    Update user form

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import UpdateUserForm from "./updateUserForm";
 
 export const metadata: Metadata = {
   title: "Update User",
@@ -16,6 +17,7 @@ const AdminUserUpdatePage = async (props: {
   return (
     <div className="space-y-8 max-w-lg mx-auto">
       <h1 className=" h3-bold">Update User</h1>
+      <UpdateUserForm user={user} />
     </div>
   );
 };
