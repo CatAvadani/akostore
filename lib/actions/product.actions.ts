ce0cc83bf883913a00939d691c05aff40004277f
CatAvadani <catalinaavadani@gmail.com>
2025-05-10 09:06:04 +0200

    Add search functionality to admin pages

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { prisma } from "@/db/prisma";
+import { Prisma } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
@@ -36,7 +37,7 @@ export async function getProductById(productId: string) {
 
 // Get all products
 export async function getAllProducts({
-  // query,
+  query,
   limit = PAGE_SIZE,
   page,
 }: // category,
@@ -46,7 +47,18 @@ export async function getAllProducts({
   page: number;
   category?: string;
 }) {
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.product.findMany({
+    where: { ...queryFilter },
     orderBy: { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
