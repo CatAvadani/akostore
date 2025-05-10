22ac818c217e2df2c77c8d760a23c122ee91aaf5
CatAvadani <catalinaavadani@gmail.com>
2025-05-10 09:47:57 +0200

    Add CategoryDrawer component

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,6 @@
 import { APP_NAME } from "@/lib/constants";
 import Link from "next/link";
+import CategoryDrawer from "./categoryDrawer";
 import Menu from "./menu";
 
 const Header = () => {
@@ -7,7 +8,8 @@ const Header = () => {
     <header className=" w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Link href="/" className=" flex-start">
+          <CategoryDrawer />
+          <Link href="/" className=" flex-start ml-2">
             <h1 className=" lg:block font-bold text-2xl ml-3">{APP_NAME}</h1>
           </Link>
           <ul className="hidden md:flex gap-6 ml-10">
